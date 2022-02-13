a929b89d9ce164bcc9337574d16feb3a13e31765
Simon <simonireilly@gmail.com>
2022-02-13 09:42:31 +0000

    WIP for injecting the bootstrap stack

---
+++ packages/cli/__tests__/bin.test.ts
@@ -0,0 +1,5 @@
+describe("implements tests to prevent regressions", () => {
+    it("has a test", () => {
+        expect(true).toEqual(true)
+    })
+})
---
+++ packages/cli/_templates/bootstrap/new/prompt.js
@@ -0,0 +1,8 @@
+module.exports = [
+    {
+        type: 'input',
+        name: 'directory',
+        message: 'Where should the aws-cdk boostrap stack be created?',
+        initial: 'packages/bootstrap',
+    },
+];
